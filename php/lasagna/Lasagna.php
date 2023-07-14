e1ff66278d27eba320fe8a3025962fb46b396cd9
Bennet Palluthe <bennet.palluthe@protonmail.com>
2023-07-14 12:21:24 +0200

    lasagne php challange

--- php/lasagna/Lasagna.php
+++ php/lasagna/Lasagna.php
@@ -4,13 +4,23 @@ declare(strict_types=1);
 
 class Lasagna
 {
-    // Please define the 'expectedCookTime()' function
+    public function expectedCookTime(): int {
+        return 40;
+    }
 
-    // Please define the 'remainingCookTime($elapsed_minutes)' function
+    public function remainingCookTime($time_in_oven): int {
+        return $this->expectedCookTime() - $time_in_oven;
+    }
 
-    // Please define the 'totalPreparationTime($layers_to_prep)' function
+    public function totalPreparationTime($layers): int {
+        return $layers * 2;
+    }
 
-    // Please define the 'totalElapsedTime($layers_to_prep, $elapsed_minutes)' function
-
-    // Please define the 'alarm()' function
+    public function totalElapsedTime($layers, $time_in_oven) {
+        return $this->totalPreparationTime($layers) + $time_in_oven;
+    }
+    
+    public function alarm(): string {
+        return "Ding!";
+    }
 }
