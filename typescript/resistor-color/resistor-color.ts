75ca10dbe527fb6eede13de48e507e1e6e1bf16a
Bennet Palluthe <bennet.palluthe@protonmail.com>
2023-07-18 11:29:52 +0200

    leap stuff

---
+++ typescript/resistor-color/resistor-color.ts
@@ -0,0 +1,14 @@
+export const colorCode = (color_name:string) => {
+  return COLORS.indexOf(color_name);
+}
+
+export const COLORS = ['black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white']
