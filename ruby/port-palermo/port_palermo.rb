43af1e5917ef45903266633930fdfa64d4d58178
Bennet Palluthe <bennet.palluthe@protonmail.com>
2023-08-29 10:57:00 +0200

    update port palermo

--- ruby/port-palermo/port_palermo.rb
+++ ruby/port-palermo/port_palermo.rb
@@ -1,11 +1,14 @@
 module Port
-  # TODO: define the 'IDENTIFIER' constant
+  IDENTIFIER = :PALE
+  PORT_A_CARGO = %w(OIL GAS).freeze
 
   def self.get_identifier(city)
-    raise 'Please implement the Port.get_identifier method'
+    city.upcase[0..3].to_sym
   end
 
   def self.get_terminal(ship_identifier)
-    raise 'Please implement the Port.get_terminal method'
+    return :A if ship_identifier.start_with?(*PORT_A_CARGO)
+
+    :B
   end
 end
